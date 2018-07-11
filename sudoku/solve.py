c090e6f39c76d3eff767614c9956c4b8fe1a7e8d
Nhan Cao <nhancv92@gmail.com>
2018-07-11 22:58:09 +0700

    refactor

--- sudoku/solve.py
+++ sudoku/solve.py
@@ -14,23 +14,14 @@ gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 # gray = cv.GaussianBlur(gray, (11, 11), 0)
 outerBox = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 5, 2)
 outerBox = cv.bitwise_not(outerBox)
-
 # cv.imshow('outerBox', outerBox)
-
-y=0
-x=3
-box = outerBox[cellH*y + padding:cellH*(y+1) - padding, cellW*x + padding:cellW*(x+1) - padding]
-# cv.imshow('box', box)
-
-# res = letter_recog.recogDigit(box)
-# print(res)
 
 for i in range(0, 9):
 	for j in range(0, 9):
 		box = outerBox[cellH*i + padding:cellH*(i+1) - padding, cellW*j + padding:cellW*(j+1) - padding]
 		res = letter_recog.recogDigit(box)
 		print(res)
-		# cv.imshow("box {0}".format(x+1), outerBox[cellH*x:cellH*(x+1), cellW*x:cellW*(x+1)])
+		# cv.imshow("box {0}x{1}".format(i, j), box)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
